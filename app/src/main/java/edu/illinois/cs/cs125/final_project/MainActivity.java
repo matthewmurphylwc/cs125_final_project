1943cfb000c159041ae09b13d2f8ace7efc61742
Matthew Murphy <mm21@illinois.edu>
2018-04-20 13:23:34 -0500

    Added percent correct calculations with nice rounding.

--- app/src/main/java/edu/illinois/cs/cs125/final_project/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/final_project/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
@@ -21,14 +23,17 @@ public class MainActivity extends AppCompatActivity {
         openFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                Log.d(TAG, "Open file button clicked");
+                Log.d(TAG, "Update button clicked");
                 EditText translateInput = (EditText)findViewById(R.id.Translate_Input);
                 EditText numberInput = (EditText)findViewById(R.id.Translation_Number);
                 String stringNumber = numberInput.getText().toString();
                 int translateNumber = Integer.parseInt(stringNumber);
                 TextView translateOutput = findViewById(R.id.Translate_Output);
+                TextView percentCorrect = findViewById(R.id.Percent_Correct);
                 translateOutput.setText(translate(translateInput.getText().toString(), translateNumber));
+                percentCorrect.setText(percentCorrect(translateInput.getText().toString(), translate(translateInput.getText().toString(), translateNumber)));
                 translateOutput.setVisibility(View.VISIBLE);
+                percentCorrect.setVisibility(View.VISIBLE);
 
             }
         });
@@ -37,7 +42,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String translate(String userInput, int userNumber) {
+        if (userNumber > 50) {
+            userNumber = 50;
+        }
         return userInput + userNumber;
+    }
+
+    private String percentCorrect(String userInput, String translateOutput) {
+        char[] userChars = userInput.toCharArray();
+        char[] translatedChars = translateOutput.toCharArray();
+        char[] fixedUser = new char [Math.max(userChars.length, translatedChars.length)];
+        char[] fixedTranslated = new char [Math.max(userChars.length, translatedChars.length)];
+        for (int i = 0; i < fixedUser.length; i++) {
+            if (i < userChars.length) {
+                fixedUser[i] = userChars[i];
+            } else {
+                fixedUser[i] = ' ';
+            }
+            if (i < translatedChars.length) {
+                fixedTranslated[i] = translatedChars[i];
+            } else {
+                fixedTranslated[i] = ' ';
+            }
+        }
+        int countCorrect = 0;
+        for (int i = 0; i < fixedUser.length; i++) {
+            Log.d(TAG, "User " + fixedUser[i]);
+            Log.d(TAG, "Translated " + fixedTranslated[i]);
+            if (fixedUser[i] == fixedTranslated[i]) {
+                countCorrect++;
+            }
+        }
+        double percentCorrect = (double)countCorrect / fixedUser.length * 100;
+        DecimalFormat percent = new DecimalFormat("###.##");
+        return percent.format(percentCorrect) + "% Match";
     }
 
 }
