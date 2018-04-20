48ac6dd7452b503f97feb18c270b0fa60e724d48
Matthew Murphy <mm21@illinois.edu>
2018-04-19 22:49:38 -0500

    Basic UI.

--- app/src/main/java/edu/illinois/cs/cs125/final_project/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/final_project/MainActivity.java
@@ -2,12 +2,39 @@ package edu.illinois.cs.cs125.final_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button openFile = findViewById(R.id.Update_Button);
+        openFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Open file button clicked");
+                EditText translateInput = (EditText)findViewById(R.id.Translate_Input);
+                TextView translateOutput = findViewById(R.id.Translate_Output);
+                translateOutput.setText(translate(translateInput.getText().toString()));
+                translateOutput.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+
     }
+
+    private String translate(String userInput) {
+        return userInput;
+    }
+
 }
