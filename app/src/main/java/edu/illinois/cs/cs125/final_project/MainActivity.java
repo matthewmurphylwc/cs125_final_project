60897c1b45dd28de503599a79e1265bf8a6f6959
Matthew Murphy <mm21@illinois.edu>
2018-04-22 16:27:22 -0500

    maybe

--- app/src/main/java/edu/illinois/cs/cs125/final_project/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/final_project/MainActivity.java
@@ -7,8 +7,42 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 import java.text.DecimalFormat;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.net.ssl.HttpsURLConnection;
+
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,6 +76,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * IDK man
+     */
+    public void startAPICall() {
+        try {
+            final JSONObject jsonBody = new JSONObject("{\"INPUT\":\"hello world\"}");
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.POST,
+                    " HTTP://API.SHOUTCLOUD.IO/V1/SHOUT",
+                    jsonBody,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+                            TextView jsonView = findViewById(R.id.jsonResult);
+                            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                            String jsonString = gson.toJson(response);
+                            jsonView.setText(jsonString);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                    Toast.makeText(getApplicationContext(), "Problem accessing API",
+                            Toast.LENGTH_LONG).show();
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    /**
      * Base function for doing translations.
      * @param userInput Whatever the user types in.
      * @param userNumber How many translations the user wants to iterate through (should be limited)
@@ -55,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
         if (userNumber > 50) {
             userNumber = 50;
         }
+
         return userInput + userNumber;
     }
 
